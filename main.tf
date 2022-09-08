57d4d10312cdd9267cfbf8b1a651569dcfcd0d6d
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-09-08 22:50:53 +0200

    update

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "azurerm_virtual_network" "this" {
 resource "azurerm_subnet" "this" {
   for_each             = var.subnets
   name                 = each.key
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  virtual_network_name = azurerm_virtual_network.this.name
   resource_group_name  = var.resource_group_name
   address_prefixes     = each.value.address_prefixes
   service_endpoints    = each.value.service_endpoints
